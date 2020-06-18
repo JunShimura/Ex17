b1e5b92c462bd7d401afe99781763ddefe8d1174
Jun Shimura <junshimura@gmail.com>
2020-06-18 15:33:08 +0900

    Scalable

--- BasicDimentionSortSystem.cs
+++ BasicDimentionSortSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Diagnostics;
 
 namespace EX17
 {
@@ -11,28 +12,38 @@ namespace EX17
             return ((new CaseInsensitiveComparer()).Compare(y, x));
         }
     }
-
-
     class Program
     {
         // 環境定義
         static readonly int dimSize = 10;   //配列の大きさ 
+
         static void Main(string[] args)
         {
-            // 配列を定義
-            int[] n = new int[dimSize];
-            // 入力
-            for (int i = 0; i < dimSize; i++)
-            {
-                Console.Write("数値を入力,{0}個目:",i+1);
-                n[i] = int.Parse(Console.ReadLine());
-            }
+            int[] n = new int[10000];
+            n[0] = 0;
+            RandomizeArray(n);
+            DisplayArray(n);
             Array.Sort(n, new ReverserClass());
-            for (int i = 0; i < dimSize; i++)
-            {
-                Console.WriteLine("{0}番目は{1}\n", i, n[i]);
-            }
-
+            DisplayArray(n);
         }
+
+        static void RandomizeArray(int[] a)
+        {
+            Random random = new Random();
+            for(int i =0;i<a.Length;i++)
+            {
+                 a[i] = random.Next();
+            }
+            return;
+        }
+        static void DisplayArray(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("[{0}]={1}", i, a[i]);
+            }
+            return;
+        }
+
     }
 }
