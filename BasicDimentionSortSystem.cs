2b162e3f2f0fb5750ceb5d915d6d15da0f7028f1
Jun Shimura <junshimura@gmail.com>
2020-06-18 14:49:39 +0900

    Array.Sort

---
+++ BasicDimentionSortSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace EX17
+{
+    public class ReverserClass : IComparer
+    {
+        // Call CaseInsensitiveComparer.Compare with the parameters reversed.
+        int IComparer.Compare(Object x, Object y)
+        {
+            return ((new CaseInsensitiveComparer()).Compare(y, x));
+        }
+    }
+
+
+    class Program
+    {
+        // 環境定義
+        static readonly int dimSize = 10;   //配列の大きさ 
+        static void Main(string[] args)
+        {
+            // 配列を定義
+            int[] n = new int[dimSize];
+            // 入力
+            for (int i = 0; i < dimSize; i++)
+            {
+                Console.Write("数値を入力,{0}個目:",i+1);
+                n[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(n, new ReverserClass());
+            for (int i = 0; i < dimSize; i++)
+            {
+                Console.WriteLine("{0}番目は{1}\n", i, n[i]);
+            }
+
+        }
+    }
+}
